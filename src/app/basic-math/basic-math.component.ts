1d08df64f6f2ab4a4d87ae4e4c30ce15ea1c2f3a
dtsivkovski <dtsivkovs@gmail.com>
2024-11-08 18:11:53 -0800

    typing and answer framework

--- src/app/basic-math/basic-math.component.ts
+++ src/app/basic-math/basic-math.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Question } from '../types/questions';
 
 @Component({
   selector: 'app-basic-math',
@@ -8,37 +9,46 @@ import { Component } from '@angular/core';
   styleUrl: './basic-math.component.scss'
 })
 export class BasicMathComponent {
-  question = generateQuestion();
-
+  question: Question = this.generateQuestion();
+  answer: number = -1;
   onInput(event: Event) {
     const value = (event.target as HTMLInputElement).value;
     console.log(value);
   }
-}
 
-const generateQuestion = () => {
-  // define operators and names
-  const operators = ['+', '-', '*', '/'];
-  const operatorNames = ['plus', 'minus', 'times', 'divided by'];
+  generateQuestion(): Question {
+    // define operators and names
+    const operators = ['+', '-', '*', '/'];
+    const operatorNames = ['plus', 'minus', 'times', 'divided by'];
 
-  // generate random operator and name
-  const randomSelect = Math.floor(Math.random() * operators.length);
-  const operator = operators[randomSelect];
-  const operatorName = operatorNames[randomSelect];
+    // generate random operator and name
+    const randomSelect = Math.floor(Math.random() * operators.length);
+    const operator = operators[randomSelect];
+    const operatorName = operatorNames[randomSelect];
 
-  // generate random numbers
-  const num1 = Math.floor(Math.random() * 100);
-  const num2 = Math.floor(Math.random() * 100);
+    // generate random numbers
+    const num1 = Math.floor(Math.random() * 100);
+    const num2 = Math.floor(Math.random() * 100);
 
-  // check for integer division and division by 0
-  if (operator === '/' && (num1 % num2 !== 0 || num2 === 0)) {
-    return generateQuestion();
+    // check for integer division and division by 0
+    if (operator === '/' && (num1 % num2 !== 0 || num2 === 0)) {
+      return this.generateQuestion();
+    }
+
+    // generate answer
+    this.answer = this.calculateAnswer(num1, operator, num2);
+
+
+    return {
+      num1,
+      operator,
+      operatorName,
+      num2
+    }
   }
 
-  return {
-    num1,
-    operator,
-    operatorName,
-    num2
+  // calculate answer
+  calculateAnswer(num1: number, operator: string, num2: number): number {
+    return -1; // TODO: implement
   }
 }
---
+++ src/app/types/questions.ts
@@ -0,0 +1,8 @@
+interface Question {
+  num1: number;
+  operator: string;
+  operatorName: string;
+  num2: number;
+}
+
+export type { Question };
